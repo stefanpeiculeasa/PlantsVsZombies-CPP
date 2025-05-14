19c58c5c606000aae0d94ec354754df636740df7
Istronomaj <stefan.peiculeasa@gmail.com>
2025-05-14 22:42:09 +0300

    Fixed warnings

--- include/wallnut.h
+++ include/wallnut.h
@@ -2,13 +2,14 @@
 #define WALLNUT_H
 
 #include "plant.h"
+#include <iostream>
 
 
 class Wallnut final : public Plant {
 public:
     Wallnut(const int x,const int y,const int scale) : Plant(x, y, 400, 5, "wallnut",120,30,scale) {}
 
-    void attack(Grid& grid) override {}
+    void attack(Grid& grid) override {std::cout << grid.getSun();}
 };
 
 
