91afc5e808c13df0b9ed051f709abe556c5baa59
Peiculeasa Sergiu-Ștefan <stefan.peiculeasa@gmail.com>
2025-05-15 18:43:00 +0300

    Update gcc_fix.h

--- include/gcc_fix.h
+++ include/gcc_fix.h
@@ -1,11 +1,11 @@
 // include/gcc_fix.h
 #pragma once
 
-// Neutralize Apple's availability macros for GCC
 #ifdef __GNUC__
+    // Disable Apple's availability macros
     #define API_AVAILABLE(...)
     #define API_UNAVAILABLE(...)
     
-    // Define missing dispatch_block_t type
-    typedef void (*dispatch_block_t)(void);
+    // Workaround for dispatch_block_t (GCD)
+    typedef void* dispatch_block_t;
 #endif
